0e09145bf30863105fb33bd23611225a104adeca
Mike Herchel <mike@lullabot.com>
2021-09-21 16:03:26 -0400

    Work on automatic carousel.

---
+++ js/carousel.js
@@ -0,0 +1,50 @@
+(() => {
+  const quoteContainer = document.querySelector('.site-testimonials__list');
+  const quotes = Array.from(quoteContainer.querySelectorAll('.site-testimonials__item'));
+
+  /**
+   * Transition the quote.
+   * @param {number} toQuoteIndex
+   */
+  function transitionQuote(toQuoteIndex) {
+    quotes.forEach(el => {
+      el.setAttribute('aria-hidden', 'true');
+      el.classList.remove('is-active');
+      el.classList.remove('is-prev');
+      el.classList.remove('is-next');
+    });
+
+    let currentQuoteIndex = toQuoteIndex;
+    if (currentQuoteIndex > quotes.length - 1) {
+      currentQuoteIndex = 0;
+    }
+
+    let prevQuoteIndex = currentQuoteIndex - 1;
+    if (prevQuoteIndex < 0) {
+      prevQuoteIndex = quotes.length - 1
+    }
+
+    let nextQuoteIndex = currentQuoteIndex + 1;
+    if (nextQuoteIndex > quotes.length - 1) {
+      nextQuoteIndex = 0;
+    }
+
+    quotes[prevQuoteIndex].classList.add('is-prev');
+    quotes[nextQuoteIndex].classList.add('is-next');
+
+    quotes[currentQuoteIndex].classList.add('is-active');
+    quotes[currentQuoteIndex].removeAttribute('aria-hidden');
+  }
+
+  function init() {
+    transitionQuote(0);
+    setInterval(() => {
+      const current = quoteContainer.querySelector('.is-active');
+      const index = quotes.indexOf(current);
+
+      transitionQuote(index + 1);
+    }, 6000);
+  }
+
+  init();
+})();
